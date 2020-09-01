5933da027d5b3f3b99a0f8aaed832e551a65615d
gitallstara <vikranthreddyaleti@gmail.com>
2020-09-01 18:43:29 -0500

    Magic 8 Ball Initial

---
+++ mb8/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MagicEightBall from './MagicEightBall'
+
+function App() {
+  return (
+    <div className="App">
+    <MagicEightBall/>
+    </div>
+  );
+}
+
+export default App;
